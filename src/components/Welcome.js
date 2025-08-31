663d2c77536e7d167928820ed054172fa913a553
Skhumbuzomasemola <skhumbuzomasemola13@gmail.com>
2025-08-31 21:12:18 +0200

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,8 @@
-import WelcomeMessage from './components/WelcomeMessage
 function WelcomeMessage() {
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <h1>Welcome to ALX React APP!</h1>
             <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
         </div>
     );
 }
